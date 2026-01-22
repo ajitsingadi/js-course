3ad7618d819394d69978170dc38f9fbfa682c561
Ajit Singadi <ajitsingadi@gmail.com>
2026-01-22 16:24:13 +0530

    coding challenge

---
+++ js6_codeChallenge/js/main.js
@@ -0,0 +1,9 @@
+/* code challenge: to give random character from your name */
+
+const anyName = "ShivaRamaKrishna";
+
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
